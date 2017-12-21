2131a34d01843558f280a8518d23cf66abaa8b78
oldlie <chsq00@163.com>
2017-12-21 21:53:49 +0800

    home page update

--- frontend/views/news/index.php
+++ frontend/views/news/index.php
@@ -6,10 +6,135 @@
 
 <div class="mod3 container clearfix">
     <div class="row">
-        <section class="col-md-8 col-sm-12">
-            文章扒拉了
+        <section class="col-md-9 col-sm-12">
+        
+        <div class="article_container">
+            
+            <div class="article_title clearfix">
+                <ul class="newhot fl">
+                    <li class="">最新</li>
+                    <li class="active">热门</li>
+                </ul>
+                <ul class="fllist fr">
+                    <li><a target="_blank" href="./sj/zx/">资讯</a></li>
+                    <li><a target="_blank" href="./sj/jd/">解读</a></li>
+                    <li><a target="_blank" href="./sj/al/">案例</a></li>
+                    <li><a target="_blank" href="./sj/ft/">访谈</a></li>
+                    <li><a target="_blank" href="./sj/hd/">活动</a></li>
+                    <li><a target="_blank" href="./xy/swzs/">实务</a></li>
+                    <li><a target="_blank" href="./sj/zc/">政策</a></li>
+                    <li><a target="_blank" href="./sj/gg/">公告</a></li>
+                </ul>
+            </div>
+            
+            <div>
+			<!--最新-->
+            <div class="article_list">
+                <div id="1" class="article_type clearfix" style="">
+                    <input type="hidden" name="chanlname" value="资讯">
+                    <div class="type1_left fl">
+                        <h1><span class="tipicon">资讯</span><a href="http://cnipr.com/sj/zx/201712/t20171220_223758.html" target="_blank">上海知产法院成立三年收案5499件 运用司法“智慧”加大对侵权制裁力度</a></h1>
+                        <div class="type1_msg">
+                            三年来，上海知产法院围绕大局，积极服务经济发展新常态，充分发挥中国法院知识产权司法保护国际交流（上海）基地的作用，为落实创新驱动发展战略，推进知识产权强国建设，推进上海加快建成具有全球影响力的科创中心建设，推进中国（上海）自由贸易试验区建设等提供了有力的司法服务和保障。
+                        </div>								
+                    </div>
+                    <div class="type1_right fr">
+                        <img src="http://cnipr.com/sj/zx/201712/W020171220593096624680.jpg">
+                    </div>
+                    <div class="clear"></div>
+                    <div class="type1_btm">
+                        <div class="time">12-20</div>
+                        <div class="yuedu"><img src="http://cnipr.com/images/yuedu.png"><span id="223758yd">阅读：502</span></div>
+                        <!--<div class="guandian"><img src="./images/guandian.png" /><span id='223758pl'>评论：6</span></div>
+
+                        <div class="dianzan"><img src="./images/dianzan.png" /><span id='223758dz'>点赞：234</span></div>-->
+                        <div class="shoucang"><img src="http://cnipr.com/images/shoucang.png"><span id="223758sc">收藏</span></div>
+                        <div onclick="javascript:doiconsearch(this);" class="tips">司法审判</div>
+                        <div onclick="javascript:doiconsearch(this);" class="tips">司法保护</div>
+                        
+                    </div>
+                </div>
+                    
+                <div id="2" class="article_type clearfix" style="">
+                    <input type="hidden" name="chanlname" value="资讯">
+                    <div class="type1_left fl">
+                        <h1><span class="tipicon">资讯</span><a href="http://cnipr.com/sj/zx/201712/t20171220_223757.html" target="_blank">国家商标基地19日开业运营 拟打造商标品牌经济</a></h1>
+                        <div class="type1_msg">
+                            商标基地是经国家工商总局批复同意建设的国家级产业园，由广东省工商局、广州市工商局和越秀区政府共同建设，于2016年12月1日与全国首个京外商标审查协作中心、商标局驻广州办事处一起落户流花展馆。
+                        </div>								
+                    </div>
+                    <div class="type1_right fr">
+                        <img src="http://cnipr.com/sj/zx/201712/W020171220589803548448.jpg">
+                    </div>
+                    <div class="clear"></div>
+                    <div class="type1_btm">
+                        <div class="time">12-20</div>
+                        <div class="yuedu"><img src="http://cnipr.com/images/yuedu.png"><span id="223757yd">阅读：506</span></div>
+                        <!--<div class="guandian"><img src="./images/guandian.png" /><span id='223757pl'>评论：6</span></div>
+
+                        <div class="dianzan"><img src="./images/dianzan.png" /><span id='223757dz'>点赞：234</span></div>-->
+                        <div class="shoucang"><img src="http://cnipr.com/images/shoucang.png"><span id="223757sc">收藏</span></div>
+                        <div onclick="javascript:doiconsearch(this);" class="tips">品牌经济</div>
+                        <div onclick="javascript:doiconsearch(this);" class="tips">商标基地</div>
+                        
+                    </div>
+                </div>
+                    
+                <div id="3" class="article_type clearfix" style="">
+                    <input type="hidden" name="chanlname" value="公告">
+                    <div class="type1_left fl">
+                        <h1><span class="tipicon">公告</span><a href="http://cnipr.com/sj/gg/201712/t20171220_223756.html" target="_blank">关于确定2017年度国家知识产权示范企业和优势企业的通知</a></h1>
+                        <div class="type1_msg">
+                            近日，国家知识产权局网站发布了关于确定2017年度国家知识产权示范企业和优势企业的通知。
+                        </div>								
+                    </div>
+                    <div class="type1_right fr">
+                        <img src="http://cnipr.com/sj/gg/201712/W020171220599333050492.png" style="display: none !important;">
+                    </div>
+                    <div class="clear"></div>
+                    <div class="type1_btm">
+                        <div class="time">12-20</div>
+                        <div class="yuedu"><img src="http://cnipr.com/images/yuedu.png"><span id="223756yd">阅读：502</span></div>
+                        <!--<div class="guandian"><img src="./images/guandian.png" /><span id='223756pl'>评论：6</span></div>
+
+                        <div class="dianzan"><img src="./images/dianzan.png" /><span id='223756dz'>点赞：234</span></div>-->
+                        <div class="shoucang"><img src="http://cnipr.com/images/shoucang.png"><span id="223756sc">收藏</span></div>
+                        <div onclick="javascript:doiconsearch(this);" class="tips">优势企业</div>
+                        <div onclick="javascript:doiconsearch(this);" class="tips">示范企业</div>
+                        
+                    </div>
+                </div>
+                    
+                <div id="4" class="article_type clearfix" style="">
+                    <input type="hidden" name="chanlname" value="解读">
+                    <div class="type1_left fl">
+                        <h1><span class="tipicon">解读</span><a href="http://cnipr.com/sj/jd/201712/t20171220_223750.html" target="_blank">高价值专利是辽宁振兴发展的助推剂</a></h1>
+                        <div class="type1_msg">
+                            加强高价值专利的转化，在运用中体现高价值。高价值专利必须得到应用才能体现其价值。
+                        </div>								
+                    </div>
+                    <div class="type1_right fr">
+                        <img src="http://cnipr.com/sj/jd/201712/W020171220343312893899.jpg">
+                    </div>
+                    <div class="clear"></div>
+                    <div class="type1_btm">
+                        <div class="time">12-20</div>
+                        <div class="yuedu"><img src="http://cnipr.com/images/yuedu.png"><span id="223750yd">阅读：520</span></div>
+                        <!--<div class="guandian"><img src="./images/guandian.png" /><span id='223750pl'>评论：6</span></div>
+
+                        <div class="dianzan"><img src="./images/dianzan.png" /><span id='223750dz'>点赞：234</span></div>-->
+                        <div class="shoucang"><img src="http://cnipr.com/images/shoucang.png"><span id="223750sc">收藏</span></div>
+                        <div onclick="javascript:doiconsearch(this);" class="tips">发明专利</div>
+                        <div onclick="javascript:doiconsearch(this);" class="tips">中国专利奖</div>
+                    </div>
+                </div>
+            </div>
+					
+					
+		</div>
+
         </section>
-        <section class="col-md-4 col-sm-12">
+        <section class="col-md-3 col-sm-12">
             别的模块啊
         </section>
     </div>
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -17,6 +17,44 @@
 </div>
 
 <div class="mod3 container clearfix">
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="product-box">
+                <img class="img-responsive" src="images/mi1.jpg">
+                <div class="product-bib">
+                    <a class="name">小米5X 变焦双摄</a>
+                    <p class="infor">变焦双摄，轻薄全金属</p>
+                    <p class="price" style="color: rgb(234, 98, 91);"><sup>¥</sup><span>1299<em>起</em></span><!----></p>
+                    <a class="btn" href="//item.mi.com/product/10000064.html" target="_blank" >立即购买</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+        <div class="product-box">
+                <img class="img-responsive" src="images/m2.jpg">
+                <div class="product-bib">
+                    <a class="name">小米Note3 人脸解锁</a>
+                    <p class="infor">1600万美颜自拍</p>
+                    <p class="price" style="color: rgb(234, 98, 91);"><sup>¥</sup><span>1999<em>起</em></span><!----></p>
+                    <a class="btn" href="//item.mi.com/product/10000064.html" target="_blank" >立即购买</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+        <div class="product-box">
+                <img class="img-responsive" src="images/m3.jpg">
+                <div class="product-bib">
+                    <a class="name">红米Note 5A 高配版</a>
+                    <p class="infor">1600万像素柔光自拍</p>
+                    <p class="price" style="color: rgb(234, 98, 91);"><sup>¥</sup><span>899<em>起</em></span><!----></p>
+                    <a class="btn" href="//item.mi.com/product/10000064.html" target="_blank" >立即购买</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="mod3 container clearfix">
     <div class="aboutUS">
         <p>
             沱湖生产的清水大闸蟹选用正宗的长江系中华绒鳌蟹苗，水草茂盛、清洁无污的沱湖为其提供了良好的生长环境，丰富的小鱼虾、螺狮等天然水生物是其食之不竭的饵料，沱湖螃蟹青背、白肚、黄毛、金爪，具有膏丰肉满、味道鲜美、营养丰富的特点。
