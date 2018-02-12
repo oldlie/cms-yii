bb5de6703ef01e08ee56b305cce057df57e4e8b5
oldlie <chsq00@163.com>
2018-02-12 16:41:29 +0800

    update cargo/form.js

--- backend/web/js/cargo/form.js
+++ backend/web/js/cargo/form.js
@@ -15,10 +15,36 @@ $(function () {
     function loadSpec(form) {
         $.post('', form, function (json) {
             if (json['success'] === 1) {
-                
+                let html = '';
+                for (let i = 0; i < json['list'].length; i++) {
+                    let item  = json['list'][i];
+                    html += core.html(trTemp, {
+                        'id': item['id'], 
+                        'title': itme['name'], 
+                        'summary' : item['feature']
+                    });
+                }
+                $('#specTableContent').html(html);
             } else {
-                callout.warning();
+                callout.warning(json['message']);
             }
         });
     };
+
+    function loadTag(formData) {
+        $.post('', formData, function (json) {
+            if (json['success'] === 1) {
+                for (let i = 0; i < json['list'].length; i++) {
+                    let item  = json['list'][i];
+                    html += core.html(trTemp, {
+                        'id': item['id'], 
+                        'title': itme['t_text'], 
+                        'summary' : item['feature']
+                    });
+                }
+            } else {
+                $('#stagTableContent').html(html);
+            }
+        });
+    }
 });
